4966953f3fffefc56d5a9f3e880f97541af1bdef
Vitalii Chechel <Vitalii_Chechel@epam.com>
2015-11-11 10:33:32 +0200

    add .gitignore

--- src/main/java/ua/twotwo/utils/AppConst.java
+++ src/main/java/ua/twotwo/utils/AppConst.java
@@ -4,7 +4,7 @@ public class AppConst {
     public static class INFO {
         public static final String BOOKING = "Booking";
         public static final String UZ = "Uz";
-        public static final String SIZE_STATIONS_AND_FROM = "GOT %s stations from %s";
+        public static final String SIZE_STATIONS_AND_FROM = "GOT %s stations from %s site";
 
     }
 

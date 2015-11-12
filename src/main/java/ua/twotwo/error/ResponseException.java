24ee01b769a2392af8a2b53fe681d038ccbc824a
Vitalii Chechel <Vitalii_Chechel>
2015-11-12 15:40:06 +0200

    custom exception

--- src/main/java/ua/twotwo/error/ResponseException.java
+++ src/main/java/ua/twotwo/error/ResponseException.java
@@ -3,7 +3,7 @@ package ua.twotwo.error;
 import org.springframework.http.ResponseEntity;
 
 import ua.twotwo.utils.AppConst;
-
+//TODO maybe it wrong ?
 public class ResponseException extends RuntimeException {
     public ResponseException(ResponseEntity response) {
         super(AppConst.EXCEPTION.BAD_RESPONSE.concat(response.toString()));

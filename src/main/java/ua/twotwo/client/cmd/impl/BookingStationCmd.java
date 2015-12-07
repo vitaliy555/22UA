084ce9d55b452f7c0f5424cc38906f1dcb61fffb
Vitalii Chechel <Vitalii_Chechel>
2015-12-07 13:14:30 +0200

    remove "/" from end of url

--- src/main/java/ua/twotwo/client/cmd/impl/BookingStationCmd.java
+++ src/main/java/ua/twotwo/client/cmd/impl/BookingStationCmd.java
@@ -5,7 +5,7 @@ import ua.twotwo.dto.booking.BookingStationAnswer;
 
 public class BookingStationCmd extends AbstractCmd {
 
-    private final String url = "http://booking.uz.gov.ua/purchase/station/";
+    private final String url = "http://booking.uz.gov.ua/purchase/station";
 
     public BookingStationCmd() {
         addHeader("Cookie", "_gv_lang=ua");

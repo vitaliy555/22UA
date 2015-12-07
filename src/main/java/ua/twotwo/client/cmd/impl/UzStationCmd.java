49855ea8138946f2ac073cd54253dd78767aa228
Vitalii Chechel <Vitalii_Chechel>
2015-12-07 13:31:12 +0200

    corrected url

--- src/main/java/ua/twotwo/client/cmd/impl/UzStationCmd.java
+++ src/main/java/ua/twotwo/client/cmd/impl/UzStationCmd.java
@@ -4,7 +4,7 @@ import ua.twotwo.client.cmd.AbstractCmd;
 
 public class UzStationCmd extends AbstractCmd {
 
-    private final String url = "http://www.uz.gov.ua/passengers/timetables/suggest-station/?q=";
+    private final String url = "http://www.uz.gov.ua/passengers/timetable/suggest-station/?q=";
 
     public UzStationCmd() {
         setUrl(url);
